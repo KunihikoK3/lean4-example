27c49c65c4b2107f5e1e172296a3ceb82ab63f77
KunihikoK3 <n_ramsamooj@hotmail.com>
2024-04-17 08:07:15 -0400

    Setup blueprint

--- lakefile.lean
+++ lakefile.lean
@@ -14,3 +14,9 @@ lean_lib «Lean4Example» {
 require mathlib from git "https://github.com/leanprover-community/mathlib4"@"master"
 require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot.git" @ "v1.2.0"
 require Paperproof from git "https://github.com/Paper-Proof/paperproof.git"@"main"/"lean"
+
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
