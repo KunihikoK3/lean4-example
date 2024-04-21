70025f53618c4f21deb2d695490415f60bf3a0ba
KunihikoK3 <n_ramsamooj@hotmail.com>
2024-04-21 07:03:46 -0400

    extensive code from LFTCM 2023 added

--- Lean4Example.lean
+++ Lean4Example.lean
@@ -28,8 +28,35 @@ section
 variable (a b : ℝ)
 
 example : (a + b) * (a + b) = a * a + 2 * (a * b) + b * b  := by
-  ring_nf
+ring
 
 
 end
 --
+theorem Euclid_Thm (n : ℕ) : ∃ p, n ≤ p ∧ Nat.Prime p := by
+  -- The proof begins by defining \( p \) as the smallest prime factor (`minFac`) of the number \( \text{factorial}(n) + 1 \)
+  let p := minFac (Nat.factorial n + 1)
+  -- `factorial_pos _` proves that `factorial n > 0`.
+  -- `Nat.succ_lt_succ <| factorial_pos _` then proves that `factorial n + 1 > 0 + 1`, or simply, `factorial n + 1 > 1`.
+ -- Finally, `Nat.ne_of_gt <| Nat.succ_lt_succ <| factorial_pos _` uses the fact that `factorial n + 1 > 1` to conclude that `factorial n + 1 ≠ 1`.
+  have f1 : factorial n + 1 ≠ 1 := Nat.ne_of_gt <| Nat.succ_lt_succ <| factorial_pos _
+  have pp : Nat.Prime p := minFac_prime f1
+  --  Less or Equal Check. Let use an example of n=10. It shows that \( n \leq p \). The code proves this by contradiction, assuming the contrary and deriving a contradiction. Specifically, it assumes that if \( p \) were less than or equal to \( n \), then \( p \) would divide \( 10! \), and thus, it would have to divide 1 (from the equation \( p \) divides \( 3628800 + 1 \)), which is impossible for a prime.
+  have np : n ≤ p :=
+    le_of_not_ge fun h =>
+      have h₁ : p ∣ factorial n := dvd_factorial (minFac_pos _) h
+      have h₂ : p ∣ 1 := (Nat.dvd_add_iff_right h₁).2 (minFac_dvd _)
+      pp.not_dvd_one h₂
+  exact ⟨p, np, pp⟩
+
+example Euclid_Thm1 (n : ℕ) : ∃ p, n ≤ p ∧ Nat.Prime p := by
+simp_all only [exists_and_right]
+apply
+  And.intro
+on_goal
+  2 =>
+  exact
+    Euclid_Thm1
+use n
+
+
