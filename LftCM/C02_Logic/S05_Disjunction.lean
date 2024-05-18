4194ac44c83469750a59f232a0b2c86bc2aee64f
KunihikoK3 <n_ramsamooj@hotmail.com>
2024-05-18 02:49:09 -0400

    logic disjunction exercises completed

--- LftCM/C02_Logic/S05_Disjunction.lean
+++ LftCM/C02_Logic/S05_Disjunction.lean
@@ -41,14 +41,6 @@ example : x < |y| → x < y ∨ x < -y := by
     rw [abs_of_neg h]
     intro h; right; exact h
 
-example : x < |y| → x < y ∨ x < -y := by
-  cases le_or_gt 0 y
-  next h =>
-    rw [abs_of_nonneg h]
-    intro h; left; exact h
-  next h =>
-    rw [abs_of_neg h]
-    intro h; right; exact h
 
 
 example : x < |y| → x < y ∨ x < -y := by
@@ -77,10 +69,35 @@ theorem neg_le_abs_self (x : ℝ) : -x ≤ |x| := by
     rw [abs_of_neg h]
 
 theorem abs_add (x y : ℝ) : |x + y| ≤ |x| + |y| := by
-  sorry
+  rcases le_or_gt 0 (x + y) with h | h
+  · rw [abs_of_nonneg h]
+    apply add_le_add
+    · apply le_abs_self
+    · apply le_abs_self
+  . rw [abs_of_neg h]
+    have h₁ : -(x + y) = - x - y := by ring
+    rw [h₁]
+    apply add_le_add
+    · apply neg_le_abs_self
+    · apply neg_le_abs_self
 
 theorem lt_abs : x < |y| ↔ x < y ∨ x < -y := by
-  sorry
+  rcases le_or_gt 0 y with h | h
+  · rw [abs_of_nonneg h]
+    apply Iff.intro
+    · intro h; left; exact h
+    · intro h
+      rcases h
+      · assumption
+      · linarith
+  . rw [abs_of_neg h]
+    apply Iff.intro
+    · intro h; right; exact h
+    · intro h
+      rcases h
+      · linarith
+      · assumption
+
 
 theorem abs_lt : |x| < y ↔ -y < x ∧ x < y := by
   sorry
@@ -98,31 +115,60 @@ example {x : ℝ} (h : x ≠ 0) : x < 0 ∨ x > 0 := by
 
 example {m n k : ℕ} (h : m ∣ n ∨ m ∣ k) : m ∣ n * k := by
   rcases h with ⟨a, rfl⟩ | ⟨b, rfl⟩
+  /-When we have a divisibility relation like m ∣ n, it means there exists some natural number a such that m * a = n. Similarly, m ∣ k means there exists some natural number b such that m * b = k.
+  The rcases tactic destructures the hypothesis h and gives names to these proof terms using the pattern ⟨a, rfl⟩ | ⟨b, rfl⟩. The rfl part of the pattern performs substitution, replacing n with m * a and k with m * b in the goal.
+  So, after the rcases tactic is applied, the goal is updated based on the case:
+    Case 1: The goal becomes m ∣ (m * a) * k, with n replaced by m * a.
+    Case 2: The goal becomes m ∣ n * (m * b), with k replaced by m * b.
+  -/
   · rw [mul_assoc]
     apply dvd_mul_right
   . rw [mul_comm, mul_assoc]
     apply dvd_mul_right
 
 example {z : ℝ} (h : ∃ x y, z = x ^ 2 + y ^ 2 ∨ z = x ^ 2 + y ^ 2 + 1) : z ≥ 0 := by
-  sorry
+  rcases h with ⟨x, y, rfl | rfl⟩
+  · linarith [pow_two_nonneg x, pow_two_nonneg y]
+  · linarith [pow_two_nonneg x, pow_two_nonneg y]
 
 example {x : ℝ} (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  have h' : (x - 1)*(x + 1) = 0 := by linarith
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h' with h'' | h''
+  · left; linarith
+  · right; linarith
 
 example {x y : ℝ} (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  have h' : (x - y)*(x + y) = 0 := by linarith
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h' with h'' | h''
+  · left; linarith
+  · right; linarith
 
+end C03S05
+/-The theorem eq_zero_or_eq_zero_of_mul_eq_zero says that the real numbers have no nontrivial zero divisors. A commutative ring with this property is called an integral domain. Your proofs of the two theorems above should work equally well in any integral domain:
+-/
 section
 variable {R : Type*} [CommRing R] [IsDomain R]
 variable (x y : R)
 
 example (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  have h' : x ^ 2 - 1 = 0 := by rw [h, sub_self]
+  have h'' : (x + 1) * (x - 1) = 0 := by
+    rw [← h']
+    ring
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h'' with h1 | h1
+  · right
+    exact eq_neg_iff_add_eq_zero.mpr h1
+  . left
+    exact eq_of_sub_eq_zero h1
+
+
 
 example (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
   sorry
 
 end
+
+example (P : Prop) : ¬¬P → P := by simp
 
 example (P : Prop) : ¬¬P → P := by
   intro h
@@ -137,4 +183,16 @@ example (P : Prop) : ¬¬P → P := by
   contradiction
 
 example (P Q : Prop) : P → Q ↔ ¬P ∨ Q := by
-  sorry
+  constructor
+  · intro h
+    by_cases h' : P
+    · right; exact h h'
+    . left; exact h'
+  · intro h
+    cases h
+    · tauto
+    · tauto
+
+/- *tauto* is a finishing tactic, meaning it either closes the goal completely or raises an error if it cannot solve the goal. It is used to dispense with propositional reasoning and can solve goals that are "obvious" propositionally.
+-/
+example (P Q : Prop) : P → Q ↔ ¬P ∨ Q := by tauto
